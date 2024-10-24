9f40539d5363d898cfa4a2737f3b5ab172e46cdd
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-10-24 12:04:14 -0500

    se ajustan cambios en el html y servicios

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CountriesService } from '../../service/countries.service';
+import { Country } from '../../interface/country';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: ``
-})
-export class CountryPageComponent {
+  styles: `
 
+  img{
+    width: 150px;
+  }
+  h2 {
+    font-size:1.5rem;
+  }
+  `
+})
+export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
+
+  constructor(
+    private countriesService: CountriesService,
+    private activateRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id)),
+      )
+      .subscribe(country => {
+        if (!country) return this.router.navigateByUrl('');
+        return this.country = country;
+
+
+
+
+      });
+  }
 }
